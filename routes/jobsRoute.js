0b4574ec799a6a1730ad15092697024426ea6b2d
deepak kumar <deepak7292832956@gmail.com>
2024-07-26 18:19:49 +0530

    created stats and done aggregation and filtered the data

--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import userAuth from '../middlewares/authMiddleware.js';
-import { createJobController, deleteJobController, getAllJobsController, updateJobController } from '../controllers/jobController.js';
+import { createJobController, deleteJobController, getAllJobsController, jobStatsController, updateJobController } from '../controllers/jobController.js';
 
 const router = express.Router();
 //Route
@@ -16,5 +16,8 @@ router.patch('/update-job/:id',userAuth,updateJobController);
 
 //DELETE JOBS || DELETE
 router.delete('/delete-job/:id',userAuth,deleteJobController);
+
+// JOBS STATS FILTER|| GET
+router.get('/job-stats',userAuth,jobStatsController);
 
 export default router;
