d340fb88832f89bd445db4e0b2a91236c6e99e9b
deepak kumar <deepak7292832956@gmail.com>
2024-07-28 17:35:51 +0530

    done pagination ,search filter, sort filter,and secured our app

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import "express-async-errors";
 import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
+//security packages
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
 //files imports
 import connectDB from './config/db.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
@@ -14,6 +18,7 @@ import testRoutes from './routes/testRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import jobsRoute from './routes/jobsRoute.js'
+import  {clean}  from 'xss-clean/lib/xss.js';
 
 // config dotenv
 dotenv.config();
@@ -26,9 +31,13 @@ const app = express();
 
 
 //middleware
+app.use(helmet());
+app.use(xss());
+app.use(ExpressMongoSanitize());
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"))
+
+app.use(morgan("dev"));
 //Routes
 app.use('/api/v1/test', testRoutes);
 app.use('/api/v1/auth',authRoutes);
